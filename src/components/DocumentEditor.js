2b860505ec502c727ed6a8aaf399f0c9847dcfa7
momarinho <117489486+momarinho@users.noreply.github.com>
2023-02-23 16:08:25 -0300

    Update DocumentEditor.js

--- src/components/DocumentEditor.js
+++ src/components/DocumentEditor.js
@@ -1,23 +1,73 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const DocumentEditor = () => {
   const [document, setDocument] = useState('');
 
+  const textareaRef = useRef(null);
+
   const handleChange = (event) => {
     setDocument(event.target.value);
+  };
+
+  const handleBold = () => {
+    const selectionStart = textareaRef.current.selectionStart;
+    const selectionEnd = textareaRef.current.selectionEnd;
+    const selectedText = document.substring(selectionStart, selectionEnd);
+    const newDocument =
+      document.substring(0, selectionStart) +
+      '<strong>' +
+      selectedText +
+      '</strong>' +
+      document.substring(selectionEnd);
+    setDocument(newDocument);
+  };
+
+  const handleItalic = () => {
+    const selectionStart = textareaRef.current.selectionStart;
+    const selectionEnd = textareaRef.current.selectionEnd;
+    const selectedText = document.substring(selectionStart, selectionEnd);
+    const newDocument =
+      document.substring(0, selectionStart) +
+      '<em>' +
+      selectedText +
+      '</em>' +
+      document.substring(selectionEnd);
+    setDocument(newDocument);
+  };
+
+  const handleUnderline = () => {
+    const selectionStart = textareaRef.current.selectionStart;
+    const selectionEnd = textareaRef.current.selectionEnd;
+    const selectedText = document.substring(selectionStart, selectionEnd);
+    const newDocument =
+      document.substring(0, selectionStart) +
+      '<u>' +
+      selectedText +
+      '</u>' +
+      document.substring(selectionEnd);
+    setDocument(newDocument);
   };
 
   return (
     <div>
       <div className="flex justify-between items-center mb-4">
         <div>
-          <button className="mr-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+          <button
+            className="mr-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+            onClick={handleBold}
+          >
             Bold
           </button>
-          <button className="mr-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+          <button
+            className="mr-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+            onClick={handleItalic}
+          >
             Italic
           </button>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+            onClick={handleUnderline}
+          >
             Underline
           </button>
         </div>
@@ -30,7 +80,9 @@ const DocumentEditor = () => {
           </button>
         </div>
       </div>
+
       <textarea
+        ref={textareaRef}
         className="w-full h-screen p-4 text-lg leading-7 rounded shadow-sm border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
         value={document}
         onChange={handleChange}
